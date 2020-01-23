2c05bf08d18353017cda1919b53f415f33102e51
Oddone330 <Nicholas_Grogg6505@hotmail.com>
2020-01-23 11:22:03 -1000

    Created a folder for the class CS450 and added the first assignment

---
+++ CS450/assignment1/GroggA1Initial.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// Function to calculate approximate value of Pi
+double piApprox(int n) {
+	// (-1^0)/(2*0+1)
+	double value = 1;
+
+	for (int it = 1; it <= n; it++) {
+		value += (pow(-1.0, it)) / (2.0*it + 1.0);
+	}
+
+	return 4 * value;
+}
+
+int main() {
+	// Instantiate variables
+	char input = 'y';
+	double pi;
+	int n;
+	cout << "Enter an integer value for n: ";
+	cin >> n;
+
+	while (input != 'n' && input != 'N') {
+		// Call function to approximate pi
+		pi = piApprox(n);
+		cout << "Pi approx. " << pi << endl;
+		cout << "Again? (y/n): ";
+		cin >> input;
+
+		if (input == 'y' || input == 'Y') {
+			cout << "Enter a new integer value for n: ";
+			cin >> n;
+		}
+	}
+
+	// I don't know if you want or need this
+	//system("pause");
+	return 0;
+}
---
+++ CS450/overview.md
@@ -0,0 +1,5 @@
+# CS450: History of Progamming languages
+
+This class dealt with progamming languages and their histories. Programs <br/>
+in this class are designed to revisit simple programming concepts or to <br/>
+replicate more recent programming functionality with older languages.
